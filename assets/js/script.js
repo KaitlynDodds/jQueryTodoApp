dd25cb684c11bb454ef26cea8c3f22eb917258e0
kadodds <kadodds@microsoft.com>
2017-10-02 16:30:18 -0700

    Fades and removes todo items in list

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,14 @@ $("li").click(function() {
     // add completed class
     $(this).toggleClass("completed");
 });
+
+// delete a todo when 'X' is clicked
+$("li span").click(function(event) {
+    // fadeout the parent li of the span (removes entire todo)
+    $(this).parent().fadeOut(1000, function() {
+        // remove li from DOM
+        $(this).remove();
+    });
+    // stop event bubbling
+    event.stopPropagation();
+});
